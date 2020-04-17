2a9e1be5e84a88f76a518897d9266111c5428604
John <laojohnmatthew@gmail.com>
2020-04-17 11:52:16 -0700

    setup proxy

--- server/index.js
+++ server/index.js
@@ -1,13 +1,40 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const port = 2985;
+const path = require("path");
+const cors = require("cors");
+const httpProxy = require("http-proxy");
 
-app.use('/', express.static(__dirname + '/../client/dist'));
+const proxy = httpProxy.createProxyServer({});
 
-app.get ('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
+app.use(cors());
+const port = 2555;
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.all('/api/listings/:listingID/reviews', (req, res) => {
+    proxy.web(req, res, {
+        target: "http://ec2-52-8-43-87.us-west-1.compute.amazonaws.com:2500"
+      });
+    } )
+
+app.all('/api/vq/p/rentals', (req, res) => {
+    proxy.web(req, res, {
+        target: "http://ec2-3-101-25-105.us-west-1.compute.amazonaws.com/"
+    });
+} )
+
+app.all('/api/listing/*', (req, res) => {
+    proxy.web(req, res, {
+        target: "http://ec2-3-133-39-202.us-east-2.compute.amazonaws.com/"
+    });
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.all('/location/*', (req, res) => {
+    proxy.web(req, res, {
+        target: "http://ec2-54-177-57-186.us-west-1.compute.amazonaws.com:3000/"
+    });
+} )
+
+app.listen(2555, () => {
+    console.log(`listening on port ${port}`);
+})
